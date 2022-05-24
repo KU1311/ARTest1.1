916ed497af8910d03adda293de9f468775916890
KU1311 <106140959+KU1311@users.noreply.github.com>
2022-05-24 17:45:21 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+window.onload = () => {
+     let places = staticLoadPlaces();
+     renderPlaces(places);
+};
+
+function staticLoadPlaces() {
+    return [
+        {
+            name: 'P1',
+            location: {
+                lat: 22.321536,
+                lng: 114.208861,
+            }
+        },
+	{
+            name: 'P2',
+            location: {
+                lat: 22.321766,
+                lng: 114.209032,
+            }
+        },
+    ];
+}
+
+function renderPlaces(places) {
+    let scene = document.querySelector('a-scene');
+
+    places.forEach((place) => {
+        let latitude = place.location.lat;
+        let longitude = place.location.lng;
+
+        let model = document.createElement('a-entity');
+        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+        model.setAttribute('gltf-model', './assets/Enero/ENERO.gltf');
+        model.setAttribute('rotation', '0 180 0');
+        model.setAttribute('scale', '1 1 1');
+
+        model.addEventListener('loaded', () => {
+            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+        });
+
+        scene.appendChild(model);
+    });
+}
